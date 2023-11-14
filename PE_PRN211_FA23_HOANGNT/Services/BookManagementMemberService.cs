794e93c4e1fbc3d07e377bc3ec6eee35ff76893f
williamHoang541 <lhh547801@gmail.com>
2023-11-14 12:00:23 +0700

    sign in button and add to data

--- PE_PRN211_FA23_HOANGNT/Services/BookManagementMemberService.cs
+++ PE_PRN211_FA23_HOANGNT/Services/BookManagementMemberService.cs
@@ -18,12 +18,12 @@ namespace Services
         //nó xử lí gì đó, có thể cần gọi Repository làm việc với DB
         //hàm của thằng này đặt tên gần với con người hơn: CheckLogin()
         //Repo đặt tên gần database hơn: Get() Create()
-
+        BookManagementMemberRepository _repo;
         public BookManagementMember? CheckLogin(string email, string password)
         {
-            BookManagementMemberRepository repo = new BookManagementMemberRepository();
+            _repo = new BookManagementMemberRepository();
 
-            BookManagementMember account = repo.Get(email); //tìm accoun theo email
+            BookManagementMember account = _repo.Get(email); //tìm accoun theo email
 
             //if (account == null)
             //    return null; //email ko tồn tai
@@ -32,7 +32,23 @@ namespace Services
             //return null;
 
             return account != null && account.Password == password ? account : null;
-        } 
+        }
+        public BookManagementMember? GetMemberByEmail(string email)
+        {
+            // fix itObject reference not set to an instance of an object.'
+            _repo = new BookManagementMemberRepository();
+
+
+            BookManagementMember newaccount = _repo.Get(email); //tìm accoun theo email
+            return newaccount;
+        }
+        public void InsertIntoDatabase(BookManagementMember member)
+        {
+            _repo = new BookManagementMemberRepository();
+
+            _repo.InsertMember(member);
+
+        }
 
     }
 }
