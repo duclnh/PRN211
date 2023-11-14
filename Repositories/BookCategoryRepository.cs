e76ad07f25535aef022ac50a19c3c0218c448e7b
Kyuma2568 <149134916+Kyuma2568@users.noreply.github.com>
2023-11-14 19:35:41 +0700

    Add files via upload

---
+++ Repositories/BookCategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    /// <summary>
+    /// Class này dùng để CRUD table Category - Thể loại Sách
+    /// Thường thể loại sách hiếm khi thay đổi, chủ yếu là lấy ra
+    /// danh sách đưa vào dropdown/combobox
+    /// </summary>
+    public class BookCategoryRepository
+    {
+        private BookManagement2023DbContext _context;
+
+        public BookCategory? Get(int CateId)
+        {
+            _context = new BookManagement2023DbContext();
+            return _context.BookCategories.Find(CateId);
+        }
+
+        public List<BookCategory> GetAll()
+        {
+            return new BookManagement2023DbContext().BookCategories.ToList();
+        }
+
+        public void Create(BookCategory cate)
+        {
+            _context = new BookManagement2023DbContext();
+            _context.BookCategories.Add(cate); 
+            _context.SaveChanges();
+        }
+        public void Update(BookCategory cate)
+        {
+            _context = new BookManagement2023DbContext();
+            _context.BookCategories.Update(cate);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Services/BookCategoryService.cs
@@ -0,0 +1,47 @@
+﻿using Repositories;
+using Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+
+    /// <summary>
+    /// Class này cung cấp dữ liệu từ table Category cho UI, và ngược lại
+    /// Class này sẽ xài CategoryRepo vừa tạo, Repo thì lại gọi đến DbContex
+    /// </summary>
+    public class BookCategoryService
+    {
+
+        private BookCategoryRepository _repo = new BookCategoryRepository();
+        public List<BookCategory> GetAllCategories()
+        {
+            _repo = new BookCategoryRepository();
+            return _repo.GetAll();
+        }
+        public List<BookCategory> SearchCategories(string keyword)
+        {
+            return _repo.GetAll().Where(b => b.BookGenreType.ToLower().Contains(keyword.ToLower()) ||
+                                             b.Description.ToLower().Contains(keyword.ToLower()) ||
+                                             b.BookCategoryId.ToString().ToLower().Contains(keyword.ToLower())).ToList();
+        }
+
+        public BookCategory? GetACategory(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public void AddACate(BookCategory cate)
+        {
+            _repo.Create(cate); 
+        }
+
+        public void UpdateACate(BookCategory cate)
+        {
+            _repo.Update(cate); 
+        }
+    }
+}
